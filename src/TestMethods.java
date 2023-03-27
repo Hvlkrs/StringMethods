15a9f975557603ba12636a707290845e3d4f9d04
Hivel K <aksoyhvl@gmail.com>
2023-03-27 11:32:37 +0200

    String methods file added, but not completed

---
+++ src/TestMethods.java
@@ -0,0 +1,9 @@
+import java.util.Locale;
+
+public class TestMethods {
+    public static void main(String[] args) {
+        String txt1 = "I LOVE JAVA";
+
+        System.out.println(txt1.toLowerCase(new Locale("Tr")));
+    }
+}
